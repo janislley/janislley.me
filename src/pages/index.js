4614e3f8f930b6aff440a6f57ed0bd7d4a2c730b
Janislley Oliveira <janislley@gmail.com>
2021-04-25 01:18:05 -0400

    new index with latest info updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,14 @@ export default function IndexPage() {
   return (
     <Layout showFooter={false}>
       <Seo
-        title="Jake Wiesler"
-        description="Front-end developer exploring human movement."
+        title="Janislley Oliveira"
+        description="I'm an Embedded SW Engineer and I mostly do Linux kernel work."
       />
       <section>
         <p sx={{m: 0, fontSize: 3, color: 'text'}}>
-          Software developer based in Orlando FL
+          Software developer based in Manaus - Brazil.
         </p>
-        <p sx={{m: 0, fontSize: 3, color: 'text'}}>Exploring human movement</p>
+        <p sx={{m: 0, fontSize: 3, color: 'text'}}>Exploring the embedded world/p>
       </section>
       <nav sx={{display: 'inline-flex', flexDirection: 'column', pt: 4}}>
         <Link sx={linkStyles} to="/blog">
@@ -30,10 +30,10 @@ export default function IndexPage() {
         <Link sx={linkStyles} to="/contact">
           /contact
         </Link>
-        <a href="https://github.com/jakewies" sx={linkStyles}>
+        <a href="https://github.com/janislley" sx={linkStyles}>
           /GitHub
         </a>
-        <a href="https://twitter.com/jakewies" sx={linkStyles}>
+        <a href="https://twitter.com/janislley" sx={linkStyles}>
           /Twitter
         </a>
       </nav>
